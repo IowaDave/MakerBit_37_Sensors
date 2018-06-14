a8dd0e616054ab09e0f6d6d281883d8e7d051b23
Iowa Dave <35478224+IowaDave@users.noreply.github.com>
2018-06-14 12:52:44 -0500

    Edit comments

--- Shake_Event.ts
+++ Shake_Event.ts
@@ -1,10 +1,10 @@
-/*****************************************
- * MakerBit Example Detect Shake Event   *
- * David Sparks  June 14, 2018           * 
- * Lights an LED connected to Pin P16    * 
- * for 1/2 of a second                   *
- * after micro:bit raises a Shake event. *
- *****************************************/
+// *****************************************
+// * MakerBit Example Detect Shake Event   *
+// * David Sparks  June 14, 2018           * 
+// * Lights an LED connected to Pin P16    * 
+// * for 1/2 of a second                   *
+// * after micro:bit raises a Shake event. *
+// *****************************************
 input.onGesture(Gesture.Shake, () => {
     pins.digitalWritePin(DigitalPin.P16, 1)
     basic.pause(500)
